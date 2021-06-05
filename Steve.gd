71bf3a1abfd9cea82d857d0e9b17092e8895ce83
racampita <56911151+racampita@users.noreply.github.com>
2021-06-05 17:43:30 +0800

    Platformer Tutorial Pt3
    +added player jump
    +added player gravity
    +note : move_and_slide() returns Vector2

--- Steve.gd
+++ Steve.gd
@@ -2,12 +2,20 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 
+const SPEED = 180
+const GRAVITY = 60
+const JUMPFORCE =-1100
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("right"):
-		velocity.x = 100
+		velocity.x = SPEED
 	if Input.is_action_pressed("left"):
-		velocity.x = -100
+		velocity.x = -SPEED
 	
-	move_and_slide(velocity)
+	velocity.y += GRAVITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMPFORCE
 	
-	velocity.x = lerp(velocity.x,0,0.1)
+	velocity = move_and_slide(velocity,Vector2.UP)
+	
+	velocity.x = lerp(velocity.x,0,0.2)

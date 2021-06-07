da80f4b07c09e15398c98be04a9a2b126266e6b4
racampita <56911151+racampita@users.noreply.github.com>
2021-06-07 12:31:52 +0800

    Platformer Tutorial Pt4
    +added animated sprites
    +added flipping of animated sprites

--- Steve.gd
+++ Steve.gd
@@ -3,14 +3,23 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 const SPEED = 180
-const GRAVITY = 60
+const GRAVITY = 40
 const JUMPFORCE =-1100
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
-	if Input.is_action_pressed("left"):
+		$Sprite.play("walk")
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$Sprite.play("walk")
+		$Sprite.flip_h = true
+	else:
+		$Sprite.play("idle")
+	
+	if not is_on_floor():
+		$Sprite.play("air")
 	
 	velocity.y += GRAVITY
 	if Input.is_action_just_pressed("jump") and is_on_floor():
